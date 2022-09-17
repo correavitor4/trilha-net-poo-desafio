7402bebc192199c3bdf345db1e7728b87e18d938
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2022-09-17 12:36:48 -0300

    Criando uma classe para auxiliar na geração de valores para atributos

---
+++ Helpers/AttributesHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace DesafioPOO.Helpers;
+
+public static class AttributesHelper
+{
+    public static string GenerateIMEI()
+    {
+        var imei = string.Empty;
+        var random = new Random();
+        for (int i = 0; i < 15; i++)
+        {
+            imei += random.Next(0, 9).ToString();
+        }
+
+        return imei;
+    }
+
+    public static int GenerateMemory(int minGbValue = 32, int MaxGbValue = 1024)
+    {
+        var random = new Random();
+        //Generate a memory size in kb
+        var memory = random.Next(minValue: minGbValue, maxValue: MaxGbValue) * 1024 * 1024;
+        return memory;
+    }
+    
+}
